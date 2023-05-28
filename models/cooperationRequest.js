421bdc7af27fb71dd5d44794a4e951e99b8d8321
vladyslav-what-is-love <vladikivacenko@gmail.com>
2023-05-28 19:39:13 +0300

    created CooperationRequest model

---
+++ models/cooperationRequest.js
@@ -0,0 +1,135 @@
+const pool = require("../db");
+const Musician = require("./musician");
+const Organizer = require("./organizer");
+
+class CooperationRequest {
+  constructor(requestData) {
+    this.request_id = requestData.request_id;
+    this.musician_id = requestData.musician_id;
+    this.organizer_id = requestData.organizer_id;
+    this.request_date = requestData.request_date;
+    this.status = requestData.status;
+  }
+
+  static async createCooperationRequest(
+    musician_id,
+    organizer_id,
+    request_date,
+    status
+  ) {
+    const query =
+      "INSERT INTO cooperation_requests (musician_id, organizer_id, request_date, status) VALUES ($1, $2, $3, $4) RETURNING *";
+    const values = [musician_id, organizer_id, request_date, status];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      const requestData = rows[0];
+      return new CooperationRequest(requestData);
+    } catch (error) {
+      throw new Error("Failed to create cooperation request");
+    }
+  }
+
+  static async getCooperationRequestById(request_id) {
+    const query = "SELECT * FROM cooperation_requests WHERE request_id = $1";
+    const values = [request_id];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      const requestData = rows[0];
+      return new CooperationRequest(requestData);
+    } catch (error) {
+      throw new Error("Failed to get cooperation request");
+    }
+  }
+
+  static async updateCooperationRequest(request_id, updates) {
+    const query =
+      "UPDATE cooperation_requests SET musician_id = $1, organizer_id = $2, request_date = $3, status = $4 WHERE request_id = $5 RETURNING *";
+    const { musician_id, organizer_id, request_date, status } = updates;
+    const values = [
+      musician_id,
+      organizer_id,
+      request_date,
+      status,
+      request_id,
+    ];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      const requestData = rows[0];
+      return new CooperationRequest(requestData);
+    } catch (error) {
+      throw new Error("Failed to update cooperation request");
+    }
+  }
+
+  static async deleteCooperationRequest(request_id) {
+    const query = "DELETE FROM cooperation_requests WHERE request_id = $1";
+    const values = [request_id];
+
+    try {
+      await pool.query(query, values);
+    } catch (error) {
+      throw new Error("Failed to delete cooperation request");
+    }
+  }
+
+  static async getRequestsByMusicianId(musician_id) {
+    const query = "SELECT * FROM cooperation_requests WHERE musician_id = $1";
+    const values = [musician_id];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      return rows.map((row) => new CooperationRequest(row));
+    } catch (error) {
+      throw new Error("Failed to get requests by musician ID");
+    }
+  }
+
+  static async getRequestsByOrganizerId(organizer_id) {
+    const query = "SELECT * FROM cooperation_requests WHERE organizer_id = $1";
+    const values = [organizer_id];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      return rows.map((row) => new CooperationRequest(row));
+    } catch (error) {
+      throw new Error("Failed to get requests by organizer ID");
+    }
+  }
+
+  static async getCooperationRequestsByStatus(status) {
+    const query = "SELECT * FROM cooperation_requests WHERE status = $1";
+    const values = [status];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      return rows.map((row) => new CooperationRequest(row));
+    } catch (error) {
+      throw new Error("Failed to get cooperation requests by status");
+    }
+  }
+
+  async getMusician() {
+    return Musician.getMusicianById(this.musician_id);
+  }
+
+  async getOrganizer() {
+    return Organizer.getOrganizerById(this.organizer_id);
+  }
+
+  static async getRequestsByDate(request_date) {
+    const query = "SELECT * FROM cooperation_requests WHERE request_date = $1";
+    const values = [request_date];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      return rows.map((row) => new CooperationRequest(row));
+    } catch (error) {
+      throw new Error("Failed to get cooperation requests by date");
+    }
+  }
+}
+
+module.exports = CooperationRequest;
--- models/organizer.js
+++ models/organizer.js
@@ -1,61 +1,58 @@
 const pool = require("../db");
+const User = require("./user");
 
 class Organizer {
-  static async createOrganizer(userId, company) {
-    const query = `
-      INSERT INTO organizers (user_id, company)
-      VALUES ($1, $2)
-      RETURNING *
-    `;
-    const values = [userId, company];
+  constructor(organizerData) {
+    this.organizer_id = organizerData.organizer_id;
+    this.user_id = organizerData.user_id;
+    this.company = organizerData.company;
+  }
+
+  static async createOrganizer(user_id, company) {
+    const query =
+      "INSERT INTO organizers (user_id, company) VALUES ($1, $2) RETURNING *";
+    const values = [user_id, company];
 
     try {
       const { rows } = await pool.query(query, values);
-      return rows[0];
+      const organizerData = rows[0];
+      return new Organizer(organizerData);
     } catch (error) {
       throw new Error("Failed to create organizer");
     }
   }
 
-  static async getOrganizerById(organizerId) {
-    const query = `
-      SELECT * FROM organizers
-      WHERE organizer_id = $1
-    `;
-    const values = [organizerId];
+  static async getOrganizerById(organizer_id) {
+    const query = "SELECT * FROM organizers WHERE organizer_id = $1";
+    const values = [organizer_id];
 
     try {
       const { rows } = await pool.query(query, values);
-      return rows[0];
+      const organizerData = rows[0];
+      return new Organizer(organizerData);
     } catch (error) {
       throw new Error("Failed to get organizer");
     }
   }
 
-  static async updateOrganizer(organizerId, updates) {
-    const { userId, company } = updates;
-    const query = `
-      UPDATE organizers
-      SET user_id = $1, company = $2
-      WHERE organizer_id = $3
-      RETURNING *
-    `;
-    const values = [userId, company, organizerId];
+  static async updateOrganizer(organizer_id, updates) {
+    const query =
+      "UPDATE organizers SET user_id = $1, company = $2 WHERE organizer_id = $3 RETURNING *";
+    const { user_id, company } = updates;
+    const values = [user_id, company, organizer_id];
 
     try {
       const { rows } = await pool.query(query, values);
-      return rows[0];
+      const organizerData = rows[0];
+      return new Organizer(organizerData);
     } catch (error) {
       throw new Error("Failed to update organizer");
     }
   }
 
-  static async deleteOrganizer(organizerId) {
-    const query = `
-      DELETE FROM organizers
-      WHERE organizer_id = $1
-    `;
-    const values = [organizerId];
+  static async deleteOrganizer(organizer_id) {
+    const query = "DELETE FROM organizers WHERE organizer_id = $1";
+    const values = [organizer_id];
 
     try {
       await pool.query(query, values);
@@ -66,14 +63,19 @@ class Organizer {
 
   static async searchOrganizersByCompany(company) {
     const query = `
-      SELECT * FROM organizers
-      WHERE LOWER(company) LIKE LOWER($1)
+      SELECT o.*, u.*
+      FROM organizers o
+      JOIN users u ON o.user_id = u.user_id
+      WHERE LOWER(o.company) LIKE LOWER($1)
     `;
     const values = [`%${company}%`];
 
     try {
       const { rows } = await pool.query(query, values);
-      return rows;
+      return rows.map((row) => ({
+        organizer: new Organizer(row),
+        user: new User(row),
+      }));
     } catch (error) {
       throw new Error("Failed to search organizers by company");
     }
