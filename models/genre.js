4918720b3e4680d83087ed63ac73f4f8d21571e9
vladyslav-what-is-love <vladikivacenko@gmail.com>
2023-05-28 19:13:43 +0300

    created genre model

---
+++ models/genre.js
@@ -0,0 +1,65 @@
+const pool = require("../db");
+
+class Genre {
+  static async createGenre(genre) {
+    const query = `
+      INSERT INTO genre (genre)
+      VALUES ($1)
+      RETURNING *
+    `;
+    const values = [genre];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      return rows[0];
+    } catch (error) {
+      throw new Error("Failed to create genre");
+    }
+  }
+
+  static async getGenreById(genreId) {
+    const query = `
+      SELECT *
+      FROM genre
+      WHERE genre_id = $1
+    `;
+    const values = [genreId];
+
+    try {
+      const { rows } = await pool.query(query, values);
+      return rows[0];
+    } catch (error) {
+      throw new Error("Failed to get genre by ID");
+    }
+  }
+
+  static async deleteGenre(genreId) {
+    const query = `
+      DELETE FROM genre
+      WHERE genre_id = $1
+    `;
+    const values = [genreId];
+
+    try {
+      await pool.query(query, values);
+    } catch (error) {
+      throw new Error("Failed to delete genre");
+    }
+  }
+
+  static async getAllGenres() {
+    const query = `
+      SELECT *
+      FROM genre
+    `;
+
+    try {
+      const { rows } = await pool.query(query);
+      return rows;
+    } catch (error) {
+      throw new Error("Failed to get all genres");
+    }
+  }
+}
+
+module.exports = Genre;
