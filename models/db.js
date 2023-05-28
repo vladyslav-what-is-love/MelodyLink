5ba9496fdb6df2748095f0226cdad672a186549b
vladyslav-what-is-love <vladikivacenko@gmail.com>
2023-05-28 16:32:17 +0300

    add gitignore

---
+++ models/db.js
@@ -0,0 +1,26 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "Vlad",
+  password: "1234",
+  host: "localhost",
+  port: "5432",
+  database: "music_search",
+});
+
+async function testDatabaseConnection() {
+  try {
+    // Виконання запиту для перевірки з'єднання
+    const client = await pool.connect();
+    console.log("Connected to the database!");
+    client.release(); // Закриття підключення
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  } finally {
+    // Закриття пула підключень
+    pool.end();
+  }
+}
+
+// Виклик функції перевірки з'єднання
+testDatabaseConnection();
