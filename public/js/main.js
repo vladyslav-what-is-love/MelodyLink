5b1b4903999b0d9eee33f3f2711bdc89ebb0c3c2
vladyslav-what-is-love <vladikivacenko@gmail.com>
2023-05-31 02:17:48 +0300

    added createUser

--- public/js/main.js
+++ public/js/main.js
@@ -169,3 +169,33 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "login.html";
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const entityInput = document.getElementById("entity-input");
+  const additionalField = document.getElementById("experience-field");
+
+  entityInput.addEventListener("change", function () {
+    const selectedValue = this.value;
+
+    if (selectedValue === "musician") {
+      additionalField.style.display = "block";
+    } else {
+      additionalField.style.display = "none";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const companyInput = document.getElementById("entity-input");
+  const additionalField = document.getElementById("company-field");
+
+  companyInput.addEventListener("change", function () {
+    const selectedValue = this.value;
+
+    if (selectedValue === "organizer") {
+      additionalField.style.display = "block";
+    } else {
+      additionalField.style.display = "none";
+    }
+  });
+});
