a04fab0d9ec039c1771dc1a8c928518b7aea30e2
vladyslav-what-is-love <vladikivacenko@gmail.com>
2023-06-07 13:54:17 +0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 var bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
